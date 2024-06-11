a21bfc5f5cde69b12814852872f1badd95056784
Hemkumar4595 <Hemkumar4595@gmail.com>
2024-06-11 05:16:29 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,13 @@
+
+const accountId=23233
+let accountEmail="Hem@gmail.com"
+let IsPortalUser=false
+let City;
+
+console.log(typeof(accountId));
+console.log(typeof(accountEmail));
+console.log(typeof(IsPortalUser));
+console.log(typeof(City));
+
+// Primitive Data Type
+// number
