1e1e0b177a840d3f7d1b45d9427d1b9d4cbc6595
Hemkumar4595 <Hemkumar4595@gmail.com>
2024-06-11 05:35:15 +0000

    data type null and undefined type of

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -4,10 +4,25 @@ let accountEmail="Hem@gmail.com"
 let IsPortalUser=false
 let City;
 
-console.log(typeof(accountId));
-console.log(typeof(accountEmail));
-console.log(typeof(IsPortalUser));
-console.log(typeof(City));
+// console.log(typeof(accountId));
+// console.log(typeof(accountEmail));
+// console.log(typeof(IsPortalUser));
+// console.log(typeof(City));
 
-// Primitive Data Type
-// number
+/** Primitive Data Type(value Type) */ 
+// 1. number
+// 2. string
+// 3. boolean
+// 4. undefined
+// 5. null
+// 6. symbol
+// 7. bigint
+
+/** Non Primitive Data Type(Reference Type) */  
+// object
+
+
+console.log(typeof(null)); // ans - object
+console.log(typeof(undefined)); // ans - undefined
+console.log(typeof(12223713137743n)); // ans - bigint
+
