62308e1cc5e280ce4281eb226f72dec8ebddbd5f
Hemkumar4595 <Hemkumar4595@gmail.com>
2024-06-11 05:38:34 +0000

    data type with define value

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -10,13 +10,13 @@ let City;
 // console.log(typeof(City));
 
 /** Primitive Data Type(value Type) */ 
-// 1. number
-// 2. string
-// 3. boolean
-// 4. undefined
-// 5. null
-// 6. symbol
-// 7. bigint
+// 1. number => 123334
+// 2. string => "Hemant"
+// 3. boolean => false
+// 4. undefined => default value
+// 5. null => standalone value
+// 6. symbol => unique
+// 7. bigint => 12223713137743n
 
 /** Non Primitive Data Type(Reference Type) */  
 // object
