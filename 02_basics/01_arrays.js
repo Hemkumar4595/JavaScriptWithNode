0faaf52ec469f1d0810f8f078348e2dce99a0e77
Hemkumar4595 <Hemkumar4595@gmail.com>
2024-06-20 16:15:14 +0000

    array basic

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+const myArr=[1,2,3,4,5]
+// console.log(myArr);
+// const myArr2=new Array(1,2,3,4,5)
+
+//1. myArr2.push(6);
+//2. myArr2.pop();
+//3. myArr2.at(1)
+//4. arr1.concat(arr2,arr3,item1)
+//5. const arr5 =Array.from(item1) // convert to any object to array for loop
+//6. const arr5 =Array.of(arr1,arr2,arr3) // make new array of multiple object
+
+//3. myArr2.shift()
+//4. myArr2.unshift(0)
+//5. myArr2.includes(4)
+//6. myArr2.indexOf(4)
+//7. myArr2.join()
+//8. myArr2.slice(-1,-3)
+//9. myArr2.splice(1,3)
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,21 @@
+const arr1=[1,2,3,4]
+ const arr2=[5,6,7,8]
+// const arr3=[9,10,11,12]
+const arrUser=['Honey','Vinod','Shyam','Krishna']
+
+// const item1="100"
+// const item2="200"
+// const item3="200"
+
+//  arr1.push(arr2)
+// const flatarr  =arr1.flat(Infinity)
+// arr1.pop();
+// arrUser.at(2) //Outpur : Shyam
+// const userName  =arrUser[2]
+// const arrConcat =arr1.concat(arr2,arr3,item1);
+// const arr4 =[...arr1,...arr2,...arr3]
+
+// const arr5 =Array.from(item1) // convert to any object to array for loop
+// const arr5 =Array.of(arr1,arr2,arr3) // make new array of multiple object
+
+console.log(flatarr);
